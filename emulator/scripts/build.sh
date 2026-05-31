cc93a393d73958bf6dbe528a7bc7ea28fff524d5
Felipe <felipe.barbosa@iugu.com>
2026-05-31 01:48:44 -0300

    Minor adjusts

--- emulator/scripts/build.sh
+++ emulator/scripts/build.sh
@@ -10,6 +10,7 @@ cp /app/config.ini /app/arcturus/target/config.ini
 mkdir -p /app/arcturus/target/plugins
 cd /app/arcturus/target/plugins
 wget -O NitroWebsockets-3.1.jar https://git.krews.org/morningstar/nitrowebsockets-for-ms/-/raw/aff34551b54527199401b343a35f16076d1befd5/target/NitroWebsockets-3.1.jar
+wget -O Camera-1.6.jar https://git.krews.org/duckietm/habbocamera/-/raw/main/Compiled/Camera-1.6.jar
 
 supervisorctl start arcturus-emulator
 
